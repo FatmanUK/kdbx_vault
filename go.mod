6da16325f8b59ed96459ff59f9fbbbebd1fa489c
Adam J. Richardson <fatman.uk@gmail.com>
2025-11-20 23:48:29 +0000

    Second commit

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module kdbx_vault
 
 go 1.24.4
+
+require (
+	github.com/tobischo/argon2 v0.1.0 // indirect
+	github.com/tobischo/gokeepasslib v1.0.0 // indirect
+	github.com/tobischo/gokeepasslib/v3 v3.6.1 // indirect
+	golang.org/x/crypto v0.45.0 // indirect
+	golang.org/x/sys v0.38.0 // indirect
+)
--- src/main.go
+++ src/main.go
@@ -1,4 +1,23 @@
 package main
 
+import (
+	"os"
+	"fmt"
+	"github.com/tobischo/gokeepasslib/v3"
+)
+
 func main() {
+    file, _ := os.Open("examples/reading/example.kdbx")
+
+    db := gokeepasslib.NewDatabase()
+    db.Credentials = gokeepasslib.NewPasswordCredentials("abcd1234#")
+    _ = gokeepasslib.NewDecoder(file).Decode(db)
+
+    db.UnlockProtectedEntries()
+
+    // Note: This is a simplified example and the groups and entries will depend on the specific file.
+    // bound checking for the slices is recommended to avoid panics.
+    entry := db.Content.Root.Groups[0].Groups[0].Entries[0]
+    fmt.Println(entry.GetTitle())
+    fmt.Println(entry.GetPassword())
 }
